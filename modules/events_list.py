70f8771111201255a7c73cbd297f662f289dd24a
Sam <samuel.fitz@btinternet.com>
2021-10-28 12:57:40 +0100

    Event Class and Events List Added

---
+++ modules/events_list.py
@@ -0,0 +1,6 @@
+from event import Event
+
+event1 = Event("2020/10/31", "Halloween", 30, "Mitchell", "Big party!!")
+event2 = Event("2020/12/17", "Christmas", 60, "Sinclair", "Last day!!!")
+
+events = [event1, event2]
